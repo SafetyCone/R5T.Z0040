996ef21ab9a3873cef9b8df0496c206b5fb1c498
davidcoats <david@getsafetycone.com>
2023-03-14 15:30:49 -0700

    Initial commit.

---
+++ source/R5T.Z0040/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0040
+{
+	/// <summary>
+	/// .NET runtime directory paths.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.Z0040/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.Z0040
+{
+    public static class Instances
+    {
+		
+    }
+}
